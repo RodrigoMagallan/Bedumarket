e51e3a1e22ae1bc9e379aaca45fc55ff91e3c758
Roydrigo <lrmagallanj513@gmail.com>
2021-03-13 01:01:21 -0600

    Se añaden controllers y models de Curso

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -1,61 +1,105 @@
-/*  Archivo controllers/cursos.js
- *  Simulando la respuesta de objetos Usuario
- *  en un futuro aquÃ­ se utilizarÃ¡n los modelos
- */
+//Declaro variables
+const db = require("../models");
+const Curso = db.curso;
+const Op = db.Sequelize.Op;
 
-// importamos el modelo de cursos
-const Curso = require('../models/Curso')
+//Crear un nuevo curso
+exports.create = (req, res) => {
+  //Función POST
+  const curso = {
+    id: req.body.id,
+    nombre: req.body.nombre,
+    url: req.body.url,
+    costo: req.body.costo,
+    descripcion: req.body.descripcion
+  };
 
-function guardarCurso(req, res, next) {
-  //Instancia del modelo Curso
-  const curs = Curso.build(req.body)
-  curs.save().then(curse => {
-    return res.status(201).json(curse.toAuthJSON())
-  }).catch(next);
-}
-
-function obtenerCurso(req, res) {
-  //Consulta a la base de datos
-Curso.findAll().then(curse => {
-  return res.json(curse)
-}).catch(error => {
-  return res.sendStatus(401)
-})
-}
-
-function modificarCurso(req, res, next) {
-const curso = Curso.create({
-  id : req.params.id,
-  ...req.body
-})
-//Guardado en la BD
-
-curs.save().then(user => {
-  return res.status(201).json(curso.toAuthJSON())
-}).catch(next);
-}
-
-function eliminarCurso(req, res) {
-// Usamos findByPK para buscar al usuario por su id
-const curs = Curso.findByPk(req.Curso.id);
-if (curs === null){
-  // si no existe lanzamos un 400 
-  return res.sendStatus(401)
-} else {
-  // Si existe, lo eliminamos
-  curs.destroy().then(curs => {
-    return res.status(200)
+  //Guardar usuario en la base de datos
+  Curso.create(curso).then(data => {
+    res.send(data);
   }).catch(err => {
-    return res.sendStatus(500)
+    res.status(500).send({
+      message: err.message || "Some error ocurred while creating the Curso."
+    });
+  });
+};
+
+//Encontrar Cursos
+
+exports.findAll = (req, res) => {
+  Curso.findAll({where:{id:{[Op.gt]: 0}}})
+    .then(data => {
+      res.send(data);
+    })
+      .catch(err => {
+        res.status(500).send({
+          message:
+          err.message  || "Some error ocurred while retrieving the data"
+        });
+      });
+};
+
+//Encontrar un usuario por ID
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Curso.findByPk(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Curso with id=" + id
+      });
+    });
+};
+
+//Actualizar un usuario por Id
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Curso.update(req.body, {
+    where: { id: id }
   })
-}
-}
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Curso was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Curso with id=${id}. Maybe Usuario was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Curso with id=" + id
+      });
+    });
+};
 
+//Borrar usuario
+exports.delete = (req, res) => {
+  const id = req.params.id;
 
-// exportamos las funciones definidas
-module.exports = {
-  guardarCurso,
-  obtenerCurso,
-  modificarCurso,
-  eliminarCurso
-}
+  Curso.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Tutorial was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Curso with id=${id}. Maybe Tutorial was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Curso with id=" + id
+      });
+    });
+};
--- models/Curso.js
+++ models/Curso.js
@@ -1,54 +1,37 @@
 // Curso.js
+module.exports = (sequelize , Sequelize) => {
 
-/** Clase que representa los cursos */
-/*class Curso{
-	constructor(id, nombre, url, costo, descripcion){
-		this.id = id; // identificador único del curso
-		this.nombre = nombre; // nombre que tendrá el curso
-		this.url = url; // url donde se encontrá el curso a impartir (dentro de la misma página)
-		this.costo = costo; // costo del curso al comprar
-		this.descripcion = descripcion; // descripción ed lo que se hará en dicho curso
-	}
-	
-}
+const Curso = sequelize.define('Curso',{
 
-module.exports = Curso;*/
-
-// importamos por separado los mÃ©todos de Sequelize y los tipos de dato.
-const { Sequelize, DataTypes } = require('sequelize');
-// importamos sequelize con la opciÃ³n de memory para forzar al gestor a almacenarla en la memoria.
-const sequelize = new Sequelize('mysql::memory:');
-
-//creamos el modelo para curso
-const Curso = sequelize.define('Curso', {
   id: {
     // se indica el tipo de dato de la columna.
-    type: DataTypes.INTEGER,
+    type: Sequelize.INTEGER,
     // indicamos que este campo es llave primaria
     primaryKey : true
   },
   nombre: {
-    type: DataTypes.STRING,
+    type: Sequelize.STRING,
     // indicamos que el campo no admite valores null
     allowNull: false
   },
   url: {
-    type: DataTypes.STRING,
+    type: Sequelize.STRING,
     // indicamos que el campo no admite valores null
     allowNull: false
   },
   costo: {
-    type: DataTypes.INTEGER,
+    type: Sequelize.STRING,
     // indicamos que el campo no admite valores null
     allowNull: false
   },
   descripcion: {
-    type: DataTypes.STRING,
+    type: Sequelize.STRING,
     // indicamos que el campo no admite valores null
     allowNull: false
   }
-  // le decimos a que tabla de nuestra base de datos corresponde.
-},{ tableName: 'Curso'});
-
-// exportamos el modelo.
-module.exports = Curso;
+},
+{
+  timestamps: false,
+});
+return Curso
+};
