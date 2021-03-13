70d5209a99b7fc47fd165902ef1f01bdd9760c65
Fernando <xgaraaxd@gmail.com>
2021-03-13 00:06:45 -0600

    cambio de funciones para el uso de sql

--- models/Usuario.js
+++ models/Usuario.js
@@ -6,22 +6,33 @@ const Usuario = sequelize.define('Usuario', {
     // se indica el tipo de dato de la columna.
     type: Sequelize.INTEGER,
     // indicamos que este campo es llave primaria
+    autoIncrement: true,
     primaryKey : true
   },
   usuario: {
     type: Sequelize.STRING,
+    // indicamos que el campo no admite valores null
+    allowNull: true
   },
    nombre: {
     type: Sequelize.STRING,
+    // indicamos que el campo no admite valores null
+    allowNull: true
   },
   email: {
     type: Sequelize.STRING,
+    // indicamos que el campo no admite valores null
+    allowNull: true
   },
   contraseña: {
     type: Sequelize.STRING,
+    // indicamos que el campo no admite valores null
+    allowNull: true
   },
   forma_de_pago: {
     type: Sequelize.STRING,
+    // indicamos que el campo no admite valores null
+    allowNull: true
   }
   },{
    timestamps: false,});
